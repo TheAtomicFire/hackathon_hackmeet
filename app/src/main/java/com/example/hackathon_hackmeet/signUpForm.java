1ef3653744b24415277fceb5fd13e1a30b7fd715
TheAtomicFire <dannyatomicfire@gmail.com>
2021-11-06 13:16:27 -0400

    try again

---
+++ app/src/main/java/com/example/hackathon_hackmeet/signUpForm.java
@@ -0,0 +1,14 @@
+package com.example.hackathon_hackmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signUpForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_form);
+    }
+}
---
+++ app/src/main/java/com/example/hackathon_hackmeet/signupOrLogin.java
@@ -0,0 +1,14 @@
+package com.example.hackathon_hackmeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signupOrLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_or_login);
+    }
+}
