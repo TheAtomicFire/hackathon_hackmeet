22b843e7cfb712146b02341e5104585b8e1e0ed5
Ruchira-Sharma <rssharma@umass.edu>
2021-11-06 21:24:52 -0400

    Updated Code

--- app/src/main/java/com/example/hackathon_hackmeet/signupOrLogin.java
+++ app/src/main/java/com/example/hackathon_hackmeet/signupOrLogin.java
@@ -2,7 +2,11 @@ package com.example.hackathon_hackmeet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+//import android.widget.EditText;
 
 public class signupOrLogin extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class signupOrLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_or_login);
+        Button b = (Button) findViewById(R.id.button5);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogIn(v);
+            }
+        });
+        Button button = (Button) findViewById(R.id.button8);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToProfileCreation(v);
+            }
+        });
     }
+    public void goToProfileCreation(View view){
+       Intent intent = new Intent(this,signUpForm.class) ;
+       startActivity(intent);
+    }
+    public void goToLogIn(View view){
+        Intent intent = new Intent(this,login_page.class) ;
+        startActivity(intent);
+    }
+
 }
+
+
