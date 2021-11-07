635acec970fc6d5a57fb8b5b612f59007e3913bb
shilpa <shilpasweth@gmail.com>
2021-11-07 05:53:55 -0500

    Asynchronous Complication

--- app/src/main/java/com/example/hackathon_hackmeet/DBLogic/AuthInfoOperations.java
+++ app/src/main/java/com/example/hackathon_hackmeet/DBLogic/AuthInfoOperations.java
@@ -2,6 +2,8 @@ package com.example.hackathon_hackmeet.DBLogic;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
 import com.example.hackathon_hackmeet.model.AuthInfo;
 import com.example.hackathon_hackmeet.model.HackerInfo;
 import com.google.firebase.database.DataSnapshot;
@@ -10,17 +12,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.CountDownLatch;
+
 public class AuthInfoOperations {
 
     private static final String TAG = AuthInfoOperations.class.getName();
+    public Boolean returnValue;
 
     public void pushAuth(AuthInfo authInfo){
         FirebaseDatabase database = FirebaseDatabase.getInstance();
 
         DatabaseReference authRef = database.getReference("authInfo/"+authInfo.getUsername());
 
-        authRef.setValue(authInfo);
+        authRef.setValue(authInfo.getPassword());
 
 
     }
+
+
 }
--- app/src/main/java/com/example/hackathon_hackmeet/login_page.java
+++ app/src/main/java/com/example/hackathon_hackmeet/login_page.java
@@ -1,33 +1,114 @@
 package com.example.hackathon_hackmeet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.hackathon_hackmeet.DBLogic.AuthInfoOperations;
+import com.example.hackathon_hackmeet.DBLogic.HackerInfoOperations;
+import com.example.hackathon_hackmeet.model.AuthInfo;
+import com.example.hackathon_hackmeet.model.HackerInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class login_page extends AppCompatActivity {
+
+    private static final String TAG = login_page.class.getName();
+    public Boolean isPasswordRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Button login = (Button) findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*SEND TEXT DATA */
+                try {
+                    Login(v);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-    public void Login(View view){
+    public void Login(View view) throws InterruptedException {
         Editable Logpassword = ((EditText) findViewById(R.id.editTextTextPassword)).getText();
+        Editable Logusername = ((EditText) findViewById(R.id.editTextTextPersonName2)).getText();
+        AuthInfo authInfo = new AuthInfo();
+        authInfo.setUsername(Logusername.toString());
+        authInfo.setPassword(Logpassword.toString());
+
+        AuthInfoOperations ops = new AuthInfoOperations();
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference authRef = database.getReference("authInfo/"+authInfo.getUsername());
+
+//        AtomicBoolean done = new AtomicBoolean(false);
+        isPasswordRight = false;
+
+            authRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    if (snapshot == null) {
+                        // The child doesn't exist
+                        Log.w(TAG, "Username doesn't exist.");
+                    } else {
+                        String dbpassword = snapshot.getValue(String.class);
+                        if (dbpassword.equals(authInfo.getPassword())) {
+                            Log.w(TAG, "Password is right.");
+                            isPasswordRight = true;
+                        }
+                    }
+//                    done.set(true);
+//                    synchronized (done) {
+//                        done.notifyAll(); // notify the main thread which is waiting
+//                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+
+//        synchronized (done) {
+//            while (done.get() == false) {
+//                done.wait(); // wait here until the listener fires
+//            }
+//        }
+
+
         TextView output = findViewById(R.id.textView2);
 //        Get String from database and chekc with the password
         String dataName =  "";
         String password = "" ;
         boolean yesName = false;
         boolean yesPassword = false;
-        if(Logpassword.toString().equals(password)){
+        if(isPasswordRight){
             yesPassword = true;
         }
-        if(yesPassword == false){
+        if(isPasswordRight == false){
             output.setVisibility(View.VISIBLE);
             output.setText("ERROR : Password Incorrect ");
         }
